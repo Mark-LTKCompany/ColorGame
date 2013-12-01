ed13ea8ecb1603dde40cb0653c3b64120c90ce5b
mark3236 <mark3236@gmail.com>
2013-12-01 23:18:03 +0900

    Added instructions

---
+++ ColorGame/InstructionViewController.h
@@ -0,0 +1,13 @@
+//
+//  InstructionViewController.h
+//  ColorGame
+//
+//  Created by iOS Developer on 2013. 12. 1..
+//  Copyright (c) 2013년 iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstructionViewController : UIViewController
+
+@end
---
+++ ColorGame/InstructionViewController.m
@@ -0,0 +1,38 @@
+//
+//  InstructionViewController.m
+//  ColorGame
+//
+//  Created by iOS Developer on 2013. 12. 1..
+//  Copyright (c) 2013년 iOS Developer. All rights reserved.
+//
+
+#import "InstructionViewController.h"
+
+@interface InstructionViewController ()
+
+@end
+
+@implementation InstructionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
