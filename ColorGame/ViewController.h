2d71645799f48812c7356a1af8274e082661165f
mark3236 <mark3236@gmail.com>
2013-12-02 20:11:02 +0900

    Method1: Following instructions from a tutorial

--- ColorGame/ViewController.h
+++ ColorGame/ViewController.h
@@ -7,8 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GameKit/GameKit.h>
+#import "GameCenterManager.h"
 
-@interface ViewController : UIViewController
+@class GameCenterManager;
 
+@interface ViewController : UIViewController <UIActionSheetDelegate, GKLeaderboardViewControllerDelegate, GKAchievementViewControllerDelegate, GameCenterManagerDelegate> {
+    GameCenterManager *gameCenterManager;
+    int64_t  currentScore;
+    NSString* currentLeaderBoard;
+    IBOutlet UILabel *currentScoreLabel;
+}
+@property (nonatomic, retain) GameCenterManager *gameCenterManager;
+@property (nonatomic, assign) int64_t currentScore;
+@property (nonatomic, retain) NSString* currentLeaderBoard;
+@property (nonatomic, retain) UILabel *currentScoreLabel;
+- (IBAction) reset;
+- (IBAction) showLeaderboard;
+- (IBAction) showAchievements;
+- (IBAction) submitScore;
+- (IBAction) increaseScore;
+- (void) checkAchievements;
 
 @end
