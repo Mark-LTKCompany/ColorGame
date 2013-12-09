5f476c970d0334d60e82b12581ca2ddf984def7e
mark3236 <mark3236@gmail.com>
2013-12-09 14:38:43 +0900

    Reset achievement-> Cancel 눌러도 초기화되는 것 bugfix

--- ColorGame/InstructionViewController.m
+++ ColorGame/InstructionViewController.m
@@ -50,34 +50,38 @@
     UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Really Reset ALL Achievements?" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"Reset Achievements" otherButtonTitles:nil];
     [actionSheet showInView:self.view];
     
-    //Local score 초기화 (게임센터는 현재 불가능)
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSNumber *scoretosave=[NSNumber numberWithInt:0];
-    [defaults setObject:scoretosave forKey:@"scoretosave"];
-    [defaults synchronize];
     
-    //Local Achievement도 초기화
-    NSNumber *under_achiever=[NSNumber numberWithInt:0];
-    [defaults setObject:under_achiever forKey:@"Under_Achiever"];
-    [defaults synchronize];
-    NSNumber *meaning_of_life=[NSNumber numberWithInt:0];
-    [defaults setObject:meaning_of_life forKey:@"Meaning_of_life"];
-    [defaults synchronize];
-    NSNumber *risk_aversion=[NSNumber numberWithInt:0];
-    [defaults setObject:risk_aversion forKey:@"Risk_Aversion"];
-    [defaults synchronize];
-    NSNumber *groovy=[NSNumber numberWithInt:0];
-    [defaults setObject:groovy forKey:@"Groovy"];
-    [defaults synchronize];
     
 }
 
 - (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
     NSString *buttonTitle = [actionSheet buttonTitleAtIndex:buttonIndex];
-    if ([buttonTitle isEqualToString:@"Reset Achievements"]) {
-        [[GameCenterManager sharedManager] resetAchievementsWithCompletion:^(NSError *error) {
+    if ([buttonTitle isEqualToString:@"Reset Achievements"])
+    {
+        [[GameCenterManager sharedManager] resetAchievementsWithCompletion:^(NSError *error)
+        {
             if (error) NSLog(@"Error Resetting Achievements: %@", error);
         }];
+        
+        //Local score 초기화 (게임센터는 현재 불가능)
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSNumber *scoretosave=[NSNumber numberWithInt:0];
+        [defaults setObject:scoretosave forKey:@"scoretosave"];
+        [defaults synchronize];
+        
+        //Local Achievement도 초기화
+        NSNumber *under_achiever=[NSNumber numberWithInt:0];
+        [defaults setObject:under_achiever forKey:@"Under_Achiever"];
+        [defaults synchronize];
+        NSNumber *meaning_of_life=[NSNumber numberWithInt:0];
+        [defaults setObject:meaning_of_life forKey:@"Meaning_of_life"];
+        [defaults synchronize];
+        NSNumber *risk_aversion=[NSNumber numberWithInt:0];
+        [defaults setObject:risk_aversion forKey:@"Risk_Aversion"];
+        [defaults synchronize];
+        NSNumber *groovy=[NSNumber numberWithInt:0];
+        [defaults setObject:groovy forKey:@"Groovy"];
+        [defaults synchronize];
     }
 }
 - (IBAction)FRENZYlabel:(id)sender {
